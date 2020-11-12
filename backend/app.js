cbd29cc3fbf4a7a4ec53d5b79d70264651e72398
Ollie <myself.ollie.lee@gmail.com>
2020-11-12 20:50:43 +1100

    add eslint

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,9 @@
-const express = require("express");
-const morgan = require("morgan");
+const express = require('express');
+const morgan = require('morgan');
 
 //middleware(sub app)
-const tourRouter = require("./routes/tourRoutes");
-const userRouter = require("./routes/userRoutes");
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
@@ -12,8 +12,8 @@ const app = express();
 // express.json() is also called body-parser
 app.use(express.json());
 
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
 
 app.use((req, res, next) => {
@@ -30,7 +30,7 @@ app.use(express.static(`${__dirname}/public`));
 // use tourRouter middleware for "/api/v1/tours" this specific route
 // we define a sub app(router) for each resource i.e. define a router for each resource
 // mounting a new router on a route
-app.use("/api/v1/tours", tourRouter);
-app.use("/api/v1/users", userRouter);
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
-const dotenv = require("dotenv");
+const dotenv = require('dotenv');
 //save environment variable in the process
 //so it can accessed in any file
-dotenv.config({ path: "./config.env" });
+dotenv.config({ path: './config.env' });
 
-const app = require("../backend/app");
+const app = require('./app');
 
 //have everything that is not related to express in one file,
 const port = process.env.PORT || 5000;
