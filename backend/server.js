4d181802fe55484c93128c480417d18816970d5f
Ollie <myself.ollie.lee@gmail.com>
2020-11-26 17:43:29 +0800

    add safe net

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,16 @@
 
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+
+//handle exception globally
+process.on('uncaughtException', (err) => {
+  console.log('Unhandled Exception!');
+  console.log(err);
+  // the code zero stands for a success
+  //  one stands for uncaught exception.
+  process.exit(1);
+});
+
 //save environment variable in the process
 //so it can accessed in any file
 dotenv.config({ path: './config.env' });
@@ -24,49 +34,25 @@ mongoose
     // console.log(connection.connections);
     console.log('DB connection successful');
   });
+
 const app = require('./app');
 
-// const tourSchema = new mongoose.Schema({
-//   //schema use native JS data type
-//   name: {
-//     type: String,
-//     required: [true, 'A tour must have name'],
-//     // we can not now have two tour documents with the same name
-//     unique: true,
-//   },
-//   rating: {
-//     type: Number,
-//     default: 4.5,
-//   },
-//   price: { type: String, required: [true, 'A tour must have price'] },
-// });
-
-// //convention in programming to always use uppercase on model names and variables.
-// //name, schema
-// const Tour = mongoose.model('Tour', tourSchema);
-
-// //use model to create a new document
-// // testTour is an instance of the tour model,
-// //so it has a couple of methods on it
-// // to interact with the database.
-// const testTour = new Tour({
-//   name: 'The Park',
-//   price: 997,
-// });
-
-// //save instance to the tours collection in the database
-// //return a promise that we can then consume, resolved value
-// // is the final document as it is in the database
-// testTour
-//   .save()
-//   .then((doc) => {
-//     console.log('doc', doc);
-//   })
-//   .catch((err) => {
-//     console.log('ERROR:', err);
-//   });
 //have everything that is not related to express in one file,
 const port = process.env.PORT || 5000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`app running on port ${port}...`);
+});
+
+//handle rejection globally
+process.on('unhandledRejection', (err) => {
+  console.log('Unhandled Rejection!');
+  console.log(err);
+  //only when server closes, will run the callback
+  //give the server time to finish all the request that are
+  // still pending or being handled at the time
+  server.close(() => {
+    // the code zero stands for a success
+    //  one stands for uncaught exception.
+    process.exit(1);
+  });
 });
