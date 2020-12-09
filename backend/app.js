8d69de000a1e61fc8c092891d15f148451807ba0
Ollie <myself.ollie.lee@gmail.com>
2020-12-09 09:15:33 +0800

    set up pug

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const path = require('path');
 
 //middleware(sub app)
 const tourRouter = require('./routes/tourRoutes');
@@ -15,10 +16,23 @@ const AppError = require('./utils/appError');
 
 const app = express();
 
+//create a template and then easily fill up that template with our data.
+app.set('view engine', 'pug');
+//our pug templates are actually called views in Express.
+//we don't always know whether a path that we receive from somewhere already has a slash or not.
+//join prevent this bug
+//express will go into "views" folder and find pug file
+app.set('views', path.join(__dirname, 'views'));
+
 // 1) global middleware
 // express.json() <- this is a middleware
 //  get access to the request body on the request object.
 // express.json() is also called body-parser
+
+//serving static files
+// app.use(express.static(`${__dirname}/public`));
+app.use(express.static(path.join(__dirname, 'public')));
+
 //read data from body into req.body
 app.use(
   express.json({
@@ -85,10 +99,14 @@ app.use((req, res, next) => {
   next();
 });
 
-//serving static files
-app.use(express.static(`${__dirname}/public`));
+// Routes
 
-// Tour Route
+//app.get, which for rendering pages in a browser is usually always the one that we use,
+app.get('/', (req, res) => {
+  // render will then render the template with the name that we pass in,
+  res.status(200).render('base');
+});
+
 // use tourRouter middleware for "/api/v1/tours" this specific route
 // we define a sub app(router) for each resource i.e. define a router for each resource
 // mounting a new router on a route
